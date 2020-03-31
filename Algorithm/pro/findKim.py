9664c08ecf3241ab282d66ad70f5372e06b87582
Lagom92 <jyyt0147@gmail.com>
2020-04-01 00:27:58 +0900

    Create findKim

---
+++ Algorithm/pro/findKim.py
@@ -0,0 +1,16 @@
+# 서울에서 김서방 찾기
+'''
+배열에서 원하는 데이터의 인덱스를 찾는
+index()를 사용함
+
+input
+["Jane", "Kim"]
+
+output
+"김서방은 1에 있다"
+'''
+
+
+def solution(seoul):
+    
+    return "김서방은 {}에 있다".format(seoul.index("Kim"))
