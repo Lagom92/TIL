420f32c6d5295dbc014847d8e7cdd958f8dad33e
Lagom92 <jyyt0147@gmail.com>
2020-05-23 16:11:02 +0900

    Update BOJ | 1920번 수 찾기

--- Algorithm/BOJ/find_number.py
+++ Algorithm/BOJ/find_number.py
@@ -17,3 +17,12 @@ for i in M_list:
         print(1)
     else:
         print(0)
+
+
+
+# 다른 사람 코드
+N, A = int(input()), {i:1 for i in map(int, input().split())}
+M = int(input())
+
+for i in list(map(int, input().split())):
+    print(A.get(i, 0))
