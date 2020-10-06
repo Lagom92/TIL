0d009d5ec9af46a61164c820af4f9fb64d94ff6e
Lagom92 <jyyt0147@gmail.com>
2020-10-06 17:31:38 +0900

    Update 프로그래머스 | 더 맵게

--- Algorithm/pro/Problem/more_spicy.py
+++ Algorithm/pro/Problem/more_spicy.py
@@ -71,3 +71,23 @@ def solution(scoville, K):
 scoville = [1, 2, 3, 9, 10, 12]
 K = 7
 print(solution(scoville, K))
+
+
+# 다른 사람 코드
+import heapq as hq
+
+def solution(scoville, K):
+
+    hq.heapify(scoville)
+    answer = 0
+    while True:
+        first = hq.heappop(scoville)
+        if first >= K:
+            break
+        if len(scoville) == 0:
+            return -1
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second*2)
+        answer += 1  
+
+    return answer
