39726853ec63a1b23c2cb9993aae0ff939979a76
Lagom92 <jyyt0147@gmail.com>
2020-10-12 22:33:30 +0900

    Creating

--- Algorithm/pro/Problem/skill_tree.py
+++ Algorithm/pro/Problem/skill_tree.py
@@ -2,44 +2,34 @@
 # https://programmers.co.kr/learn/courses/30/lessons/49993#fnref1
 '''
 
-미완
-'''
+14개 중 8개 맞춤
 
+이유가 머지...??
+'''
 
 def solution(skill, skill_trees):
     res = 0
-    for skill_tree in skill_trees:
-        box = list(skill)
-        i = 0
-        while skill_tree:
-            t = skill_tree.pop(0)
-            if i < len(box):
-                if t == box[i]:
-                    i += 1
-                else:
-                    if t in box:
-                        idx = box.index(t)
-                        
-
-            else:
-                pass
-            
-
-
-            else:
-                pass
-                
-
-            
-        
-
-
-
+    lst = list(skill)
+    for tree in skill_trees:
+        box = ""
+        # print(tree)
+        for t in tree:
+            # print(t)
+            if t in lst:
+                # print(box)
+                box += t
+        if len(box):
+            if box[0] == lst[0]:
+                print(box)
+                res += 1
+        else:
+            res += 1
 
     return res
 
 
 skill = "CBD"
+# skill_trees = ["BACDE", "CBADF", "AECB", "BDA", "CXF", "ASF", "BDF", "CEFD"]
 skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
 # res = 2
 
